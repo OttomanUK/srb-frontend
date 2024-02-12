96f08d8cab4f4e89fe81f1a3e782ae6c571fff1e
Your Name <you@example.com>
2024-02-12 21:32:26 +0500

    Small fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -30,16 +30,10 @@ function Dashboard() {
 
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-
             {/* Welcome banner */}
             <WelcomeBanner />
-
             {/* Dashboard actions */}
             <div className="sm:flex sm:justify-between sm:items-center mb-8">
-
-              {/* Left: Avatars */}
-              {/* <DashboardAvatars /> */}
-
               {/* Right: Actions */}
               <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
                 {/* Filter button */}
