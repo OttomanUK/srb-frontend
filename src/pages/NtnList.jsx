92983f3eb7c1be8285a889d8535082cde8f4c0d7
mohib <qureshimohib819@gmail.com>
2024-03-04 23:18:28 +0500

    added error page

--- src/pages/NtnList.jsx
+++ src/pages/NtnList.jsx
@@ -9,6 +9,7 @@ import { getAllNtn } from '../action/action';
 import Loader from '../components/utils/Loader';
 import Footer from '../components/dashboard_components/DashboardFooter';
 import PleaseReload from './PleaseReload';
+
 import NotFound from './NotFound';
 function useQuery() {
   return new URLSearchParams(useLocation().search);
@@ -17,6 +18,7 @@ function NtnList() {
   const customGreeting = 'NTN Lookup';
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const query =useQuery()
+  const [error,setError]=useState(false)
   const page=parseInt(query.get('page'))||1
   const ntn=query.get('ntn')||"all"
   const { isLoading } = useSelector(state => state.centralStore);
@@ -30,36 +32,44 @@ function NtnList() {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setError(false)
         const { results, count } = await dispatch(getAllNtn(page));
         setFilteredData(results);
         setData(results);
         setCount(count);
       } catch (error) {
         console.error("Error fetching data:", error);
-        navigate('/NotFound'); // Redirect to the not-found page or handle the error appropriately
+        setError(true)
       }
     };
   
-    fetchData().catch((error) => {
-      console.error("Error in fetchData function:", error);
-    });
+    fetchData()
   }, [page]);
 
     // Filter data based on searchInput
     useEffect(() => {
+      try{
+
         // Filter data based on searchInput
         const filteredResults = data.filter((item) => {
           const ntnString = String(item.ntn); // Convert ntn to string
           return (
             (ntnString.toLowerCase().includes(searchInput.toLowerCase())) ||
             (typeof item.name === 'string' && item.name.toLowerCase().includes(searchInput.toLowerCase()))
-          );
+            );
+          
         });
         setFilteredData(filteredResults);
+      }catch(err){
+        setError(true)
+      }
       }, [searchInput, data]);
 
+      if(error){
+        return <PleaseReload/>
+      }
   if (isLoading) {
-    return <Loader />;
+  return <Loader />;
   }
 
 
